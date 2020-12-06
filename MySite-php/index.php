ea778eaf6f206d47d5e080f73cac6f832c0ff08f
alfystar <alfystar1701@gmail.com>
2020-12-06 22:50:51 +0100

    biografia

--- MySite-php/index.php
+++ MySite-php/index.php
@@ -50,24 +50,85 @@
         <div class="row">
             <div class="col-xs-12 col-md-8 col-md-offset-2">
                 <div class="text-center">
-                    <h2>About Me</h2>
-                    <p class="text-justify">
-                        Fin da piccolo sono stato un grande appassionato di elettronica e fai-da-te. Crescendo mi sono
-                        avvicinato al mondo dell'elettronica hobbystica e digitale, con un occhio verso la
-                        programmazione.
-                        Grazie al percorso di studi che ho scelto di seguire, andando al tecnico industriale "Galileo
-                        Galilei" ho avuto modo di coltivare le mie passioni e ho scoperto Arduino e il mondo embedded
-                        dei Micro-Controllori.
+                    <h2>Biografia</h2>
+                    <h3>Percorso Formativo</h3>
+                    <p class="text-left">
+                        Attualmente seguo, presso <a href="#" target="Univeristà"><b>Università di Tor Vergata</b></a>, il corso magistrale in <b>Ingengeria dell'Automazione</b>.
                         <br><br>
-                        Sono entrato nel gruppo di Robotica della mia Scuola, e questo mi ha permesso di provare subito
-                        e con mano tutto quello che ci veniva spiegato in classe.
-                        In seguito sono entrato in contatto con il FabLab del Quadraro, lì, oltre ad aver conosciuto
-                        delle personi importanti ho avuto l'occasione di scoprire e imparare a usare macchine a
-                        controllo numerico come CNC, Stampanti 3D e Tagliatrici Laser.
+                        Sempre a <a href="#" target="Univeristà"><b>Tor Vergata</b></a> ho conseguito
+                        la laurea in <b>Ingengeria Informatica ed Automatica</b> con la <b>votazione di 110 e Lode</b>.<br>
+                        Tra i crediti facoltativi ho inserito degli esami <i>Vulnerabilità e Sicurezza Informatica</i> per la
+                        grande curiosità che questi temi suscitano sempre in me.
                         <br><br>
-                        Ora sono all'università e grazie agli studi che ho fatto non affronto più il mondo come un Maker
-                        appassionato, bensì come un Ingengere preparato.
+                        Alle superiori sono stato iscritto all'<a href="http://www.itisgalilei.edu.it/it/" target="Superiori"><b>ITIS Galielo Galilei</b></a>,
+                        indirizzo: <b>Perito Elettronico ed Automatico</b> mi sono diplomato con <b>voto di Maturita di 100/100</b>.
+
                     </p>
+                    <h3>Competizioni ed Eventi</h3>
+                    <p class="text-left">
+                        Sin dagli anni delle superiori ho avuto attività extra scolastiche molto immersive, che mi hanno
+                        portato a partecipare a molti eventi, ecco una lista <i>non esaustiva</i> degli eventi a
+                        cui ho partecipato:
+                        <ul class="text-left blockList">
+                        <li>
+                            <div class="flex-conteiner">
+                                <p class="flex-child">
+                                    [12-14/10/2018] Espositore alla <b>Maker Faire Rome 2018</b>, con la versione <b>FINALE</b>
+                                    del 3D-Robot-Pac-Man.
+                                </p>
+                                <a class="btn btn-info btn-sm pull-right flex-child-Small" target="giornali"
+                                   href="https://magpi.raspberrypi.org/articles/robot-pac-man">
+                                    Learn More
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            [01-03/12/2017] Espositore alla <b>Maker Faire Rome 2017</b>, della versione ulteriormente migliorata del 3D-Robot-Pac-Man
+                        </li>
+                        <li>
+                            [14-16/10/2016] Espositore alla <b>Maker Faire Rome 2016</b>, con il progetto di gruppo del 3D-Robot-Pac-Man
+                        </li>
+                        <li>
+                            [16-18/10/2015] Espositore alla <b>Maker Faire Rome 2015</b> dei robot di robotica (con i quali lo stesso anno vincemmo i mondiali)
+                            e del mio progetto personale estivo, il <a href="#">Disco Laser</a>.
+                        </li>
+                        <li>
+                            <div class="flex-conteiner">
+                                <p class="flex-child">
+                                    [17-23/07/2015] Vincitore della <b>RoboCup-Junior sezione Soccer Light</b> in Cina.
+                                </p>
+                                <a class="btn btn-info btn-sm pull-right flex-child-Small" target="giornali"
+                                   href="https://www.italia-news.it/roma-iis-galileo-galilei-vince-i-mondiali-di-robotica-in-cina-11553.html#:~:text=robotica%20in%20Cina-,Roma.,costruiti%20e%20programmati%20dagli%20studenti.">
+                                    Learn More
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            [19-25/06/2014] Secondo posto alla  <b>RoboCup-Junior sezione Soccer Light</b>  in Brasile.
+                        </li>
+                        <li>
+                            [12-13/4/2014] Secondo posto della  <b>Space Apps Rome 2014</b>  con il progetto
+                            <a href="http://www.davidealoisi.it/eventi/spaceapps/" target="_blank">MOSIMOV</a>.
+                        </li>
+                        </ul>
+                    </p>
+<!--                    <p class="text-justify">-->
+<!--                        Fin da piccolo sono stato un grande appassionato di elettronica e fai-da-te. Crescendo mi sono-->
+<!--                        avvicinato al mondo dell'elettronica hobbystica e digitale, con un occhio verso la-->
+<!--                        programmazione.-->
+<!--                        Grazie al percorso di studi che ho scelto di seguire, andando al tecnico industriale "Galileo-->
+<!--                        Galilei" ho avuto modo di coltivare le mie passioni e ho scoperto Arduino e il mondo embedded-->
+<!--                        dei Micro-Controllori.-->
+<!--                        <br><br>-->
+<!--                        Sono entrato nel gruppo di Robotica della mia Scuola, e questo mi ha permesso di provare subito-->
+<!--                        e con mano tutto quello che ci veniva spiegato in classe.-->
+<!--                        In seguito sono entrato in contatto con il FabLab del Quadraro, lì, oltre ad aver conosciuto-->
+<!--                        delle personi importanti ho avuto l'occasione di scoprire e imparare a usare macchine a-->
+<!--                        controllo numerico come CNC, Stampanti 3D e Tagliatrici Laser.-->
+<!--                        <br><br>-->
+<!--                        Ora sono all'università e grazie agli studi che ho fatto non affronto più il mondo come un Maker-->
+<!--                        appassionato, bensì come un Ingengere preparato.-->
+<!--                    </p>-->
                 </div>
             </div>
         </div>
@@ -79,11 +140,7 @@
         <div class="row">
             <div class="col-xs-12 col-md-12">
                 <div class="section-container-spacer">
-                    <h2 class="text-center">Contattatemi</h2>
-                    <p class="text-center">
-                        Per entrare in contatto con me scrivetemi pure una email
-                        o raggiungetemi su uno dei miei social
-                    </p>
+                    <h2 class="text-center">Contatti</h2>
                 </div>
                 <div class="card-container">
                     <div class="card card-shadow col-xs-10 col-xs-offset-1 col-md-12 col-md-offset-0 reveal">
